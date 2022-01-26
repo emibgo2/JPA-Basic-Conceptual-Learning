6ab72e71d37aa132faf1348084e834232fd8a197
emibgo2 <emibgo2@naver.com>
2022-01-26 22:54:21 +0900

    JPA Basic Conceptual Learning Start!
    - Problems with SQL-oriented development, JPA, Persistence context, Flush, Entity life cycle.
    
    JPA Basic Conceptual Learning Start + 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# JPA Basic
+
+## JPA Basic conceptual learning, Java Standard ORM 
+- Problems with SQL-oriented development, JPA, Persistence context, Flush, Entity life cycle.
+
+reference: [김영한님의 자바 ORM 표준 JPA 프로그래밍 - 기본편](https://www.inflearn.com/course/ORM-JPA-Basic/dashboard) 
---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // 영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("==============");
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("erro!");
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}

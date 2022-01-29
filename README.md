9d33c5307c689204a1a378793b7634e810bf31ad
emibgo2 <emibgo2@naver.com>
2022-01-29 21:10:28 +0900

    Entity Mapping - Object to table mapping - DB schema auto create - Field to column mapping - Primary key mapping - Requirement analysis and basic mapping
    JPA Basic Conceptual Learning Start + 2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JPA Basic
 
 ## JPA Basic conceptual learning, Java Standard ORM 
-- Problems with SQL-oriented development, JPA, Persistence context, Flush, Entity life cycle.
+- Problems with SQL-oriented development, JPA, Persistence context, Flush, Entity life cycle, Object to table mapping, DB schema auto create, Field and column mapping, Primary key mapping, Requirement analysis and basic mapping.
 
 reference: [김영한님의 자바 ORM 표준 JPA 프로그래밍 - 기본편](https://www.inflearn.com/course/ORM-JPA-Basic/dashboard) 
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
+import hellojpa.jpashop.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -18,13 +19,7 @@ public class JpaMain {
 
         try {
 
-            // 영속
-            Member member = new Member(200L, "member200");
-            em.persist(member);
 
-            em.flush();
-
-            System.out.println("==============");
             tx.commit();
         } catch (Exception e) {
             System.out.println("erro!");

3e5c180da31c44c0951f4e6043511c20b6723104
emibgo2 <emibgo2@naver.com>
2022-01-31 23:45:03 +0900

    Various Relationship mapping - N:1, 1:N, 1:1, N:M
    JPA Basic Conceptual Learning Start + 4

---
+++ src/main/java/hellojpa/jpashop/Delivery.java
@@ -0,0 +1,20 @@
+package hellojpa.jpashop;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+}
